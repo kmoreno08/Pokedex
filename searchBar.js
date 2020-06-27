b04d1c559cd4070185bb01f3ff418565b51a66a8
Kevin Moreno <moren162@mail.chapman.edu>
2020-06-27 08:50:14 -0700

    Search Bar - specific pokemon

--- searchBar.js
+++ searchBar.js
@@ -1,16 +1,29 @@
 const search = document.getElementById("search");
 const matchList = document.getElementById("match-list");
+// const searchCard = document.querySelector(".searchcard");
 let pokeNameArray = [];
 
 const fetchAllPokemons = async (searchText) => {
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= pokemons_number; i++) {
     await searchPokemon(i);
   }
   let matches = pokeNameArray.filter((poke) => {
     const regex = new RegExp(`^${searchText}`, "gi");
     return poke.match(regex);
   });
+
+  // Show results in HTML
   let pokeSearchArray = removeDuplicates(matches);
+  // console.log(pokeSearchArray);
+  let html = "";
+  pokeSearchArray.forEach((pokemon) => {
+    html += `
+      <div class="searchCard" onclick="pickPokemon(this)">
+       <h4>${pokemon}</h4>
+       </div>`;
+  });
+
+  matchList.innerHTML = html;
 };
 
 const searchPokemon = async (id) => {
@@ -24,5 +37,19 @@ const searchPokemon = async (id) => {
 function removeDuplicates(array) {
   return Array.from(new Set(array));
 }
-
 search.addEventListener("input", () => fetchAllPokemons(search.value));
+
+// Click on searchcard to display specific pokemon
+function pickPokemon(e) {
+  let pokemonChoice = e.firstElementChild.firstChild.data;
+  getSpecificPokemon(pokemonChoice);
+}
+
+// Grab specific pokemon
+const getSpecificPokemon = async (name) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  console.log(pokemon.name);
+  createPokemonCard(pokemon);
+};
